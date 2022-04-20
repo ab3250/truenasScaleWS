dd2204ee5be4c2f78171f7aca1db39d632cd3e93
ab <ab3250@live.com>
2022-04-20 08:35:38 -0400

    2022_04_20_08_35_AM

--- jsbrowser/assets/js/callbacks.js
+++ jsbrowser/assets/js/callbacks.js
@@ -3,61 +3,60 @@ let sessionId = 111
 document.addEventListener('DOMContentLoaded', loadWindow, false)
 function loadWindow () {
 }
-//const connectStr = JSON.stringify({"msg": "connect","version": "1","support": ["1"]})
-//const loginStr = JSON.stringify({"id":sessionId,"msg":"method","method":"auth.login","params": ["root","hendrix1942"]})
+// const connectStr = JSON.stringify({"msg": "connect","version": "1","support": ["1"]})
+// const loginStr = JSON.stringify({"id":sessionId,"msg":"method","method":"auth.login","params": ["root","hendrix1942"]})
 
-function connectCb(evt) {
-    console.log("recv message" + evt.data)
-    let jsonData=JSON.parse(evt.data)
+function connectCb (evt) {
+  console.log('recv message' + evt.data)
+  const jsonData = JSON.parse(evt.data)
+  sessionId = jsonData.session
+  console.log('444' + jsonData.session)
+  if (jsonData.msg === 'connected') {
+    statusConnected = true
     sessionId = jsonData.session
-    console.log("444" + jsonData.session)
-    if (jsonData.msg === "connected"){
-            statusConnected = true
-            sessionId = jsonData.session
-            console.log(jsonData.session)
-    } else if (jsonData.msg === "failed"){
-    console.log("error")
-    }
-    console.log("waiting")
-    waitForResponse()
-    console.log("done waiting")
+    console.log(jsonData.session)
+  } else if (jsonData.msg === 'failed') {
+    console.log('error')
   }
-  
-function loginCb(evt) {
-    let jsonData=JSON.parse(evt.data)
-    console.log(jsonData)
-    if (jsonData.msg === "connected"){
-                  statusConnected = true
-                   sessionId = jsonData.session
-                    console.log(jsonData.session)
-                    } else if (jsonData.msg === "failed"){
-        console.log("error")
-          }
-          console.log("more waiting")
-        waitForResponse()
-        console.log("more done waiting")
-          }
+  console.log('waiting')
+  waitForResponse()
+  console.log('done waiting')
+}
 
+function loginCb (evt) {
+  const jsonData = JSON.parse(evt.data)
+  console.log(jsonData)
+  if (jsonData.msg === 'connected') {
+    statusConnected = true
+    sessionId = jsonData.session
+    console.log(jsonData.session)
+  } else if (jsonData.msg === 'failed') {
+    console.log('error')
+  }
+  console.log('more waiting')
+  waitForResponse()
+  console.log('more done waiting')
+}
 
-          //	console.log("p2 start")	
-	//	getPromiseFromEvent(ws, "onmessage", loginStr).then(console.log("p2 end"))
+//	console.log("p2 start")
+//	getPromiseFromEvent(ws, "onmessage", loginStr).then(console.log("p2 end"))
 
-		//sendRequest(ws,loginStr, loginCb)
+// sendRequest(ws,loginStr, loginCb)
 
-		// sendRequest(ws,"onmessage",JSON.stringify({"id":"d8e715be-6bc7-11e6-8c28-00e04d680384","msg":"method",
-		// 			  "method":"auth.login","params": ["root","hendrix1942"]})
-		// 			   ,function(evt) {
-		// 			 let jsonData=JSON.parse(evt.data)
-		// 			 console.log(jsonData)
-		// 			 if (jsonData.msg === "connected"){
-		// 						   statusConnected = true
-		// 							sessionId = jsonData.session
-		// 							 console.log(jsonData.session)
-		// 							 } else if (jsonData.msg === "failed"){
-		// 				 console.log("error")
-		// 				   }
-		// 				   console.log("more more waiting")
-		// 				 waitForResponse()
-		// 				 console.log("more more done waiting")
-		// 				   })	  
-	ws.close()
+// sendRequest(ws,"onmessage",JSON.stringify({"id":"d8e715be-6bc7-11e6-8c28-00e04d680384","msg":"method",
+// 			  "method":"auth.login","params": ["root","hendrix1942"]})
+// 			   ,function(evt) {
+// 			 let jsonData=JSON.parse(evt.data)
+// 			 console.log(jsonData)
+// 			 if (jsonData.msg === "connected"){
+// 						   statusConnected = true
+// 							sessionId = jsonData.session
+// 							 console.log(jsonData.session)
+// 							 } else if (jsonData.msg === "failed"){
+// 				 console.log("error")
+// 				   }
+// 				   console.log("more more waiting")
+// 				 waitForResponse()
+// 				 console.log("more more done waiting")
+// 				   })
+ws.close()
